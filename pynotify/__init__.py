1f020088a7a669db890c30dc781cadf075dc7353
Ondřej Kulatý <kulaty.o@gmail.com>
2019-04-12 08:59:32 +0200

    Bump version: 0.1.4 → 0.1.5

--- pynotify/__init__.py
+++ pynotify/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Ondřej Kulatý"""
 __email__ = 'kulaty.o@gmail.com'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     name='django-pynotify',
     packages=find_packages(include=['pynotify']),
     url='https://github.com/druids/django-pynotify',
-    version='0.1.4',
+    version='0.1.5',
     zip_safe=False,
 )
