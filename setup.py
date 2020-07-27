62a222419acff286f6632037854ff09879d03101
Ondrej Kulaty <kulaty.o@gmail.com>
2020-07-27 02:29:31 +0200

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     name='django-pynotify',
     packages=find_packages(include=['pynotify']),
     url='https://github.com/druids/django-pynotify',
-    version='0.3.1',
+    version='0.3.2',
     zip_safe=False,
 )
