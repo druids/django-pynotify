444b01a3ac16f47ba73506f0061f81fadb6dcc0a
Ondrej Kulaty <kulaty.o@gmail.com>
2021-01-21 13:59:54 +0100

    Bump version: 0.4.4 → 0.4.5

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     name='django-pynotify',
     packages=find_packages(include=['pynotify']),
     url='https://github.com/druids/django-pynotify',
-    version='0.4.4',
+    version='0.4.5',
     zip_safe=False,
 )
