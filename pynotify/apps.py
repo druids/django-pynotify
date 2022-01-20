1943157f29ca04d143e7615662324cef17ba1068
Ondrej Kulaty <kulaty.o@gmail.com>
2022-01-20 13:54:29 +0100

    Migrate ID fields

--- pynotify/apps.py
+++ pynotify/apps.py
@@ -6,6 +6,7 @@ from .helpers import autoload
 class PyNotifyConfig(AppConfig):
     name = 'pynotify'
     verbose_name = 'PyNotify'
+    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
         from .notify import NotifyHandler  # noqa
